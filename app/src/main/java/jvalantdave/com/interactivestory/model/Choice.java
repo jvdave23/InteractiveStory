42fc1c1b8978e8cd41300ae9ec360279e361d4fe
pivotal-jvalant-dave <jdave@pivotal.io>
2015-12-01 16:32:07 -0500

    get and set ya maa

---
+++ app/src/main/java/jvalantdave/com/interactivestory/model/Choice.java
@@ -0,0 +1,25 @@
+package jvalantdave.com.interactivestory.model;
+
+/**
+ * Created by JvalantDave on 2015-12-01.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
---
+++ app/src/main/java/jvalantdave/com/interactivestory/model/Page.java
@@ -0,0 +1,43 @@
+package jvalantdave.com.interactivestory.model;
+
+/**
+ * Created by JvalantDave on 2015-12-01.
+ */
+public class Page {
+    private int mImageID;
+    private String mText;
+    private Choice mChoice1;
+    private Choice mChoice2;
+
+    public int getImageID() {
+        return mImageID;
+    }
+
+    public void setImageID(int imageID) {
+        mImageID = imageID;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public Choice getChoice1() {
+        return mChoice1;
+    }
+
+    public void setChoice1(Choice choice1) {
+        mChoice1 = choice1;
+    }
+
+    public Choice getChoice2() {
+        return mChoice2;
+    }
+
+    public void setChoice2(Choice choice2) {
+        mChoice2 = choice2;
+    }
+}
