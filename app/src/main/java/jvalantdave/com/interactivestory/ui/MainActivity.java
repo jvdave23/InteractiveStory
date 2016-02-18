8f6023aa3e81e2c83b9adda81b41ee3954346f85
jvdave23 <jvdave23@gmail.com>
2016-02-18 17:44:34 -0500

    Final

--- app/src/main/java/jvalantdave/com/interactivestory/ui/MainActivity.java
+++ app/src/main/java/jvalantdave/com/interactivestory/ui/MainActivity.java
@@ -43,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //mNameField.setText("");
+    }
 }
 
 
